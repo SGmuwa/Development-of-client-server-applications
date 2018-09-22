dd50aa94571267f6f65cb6e3f5b49af05acd4620
SG_Muwa <motherlode.muwa@gmail.com>
2018-09-22 09:14:31 +0300

    Первая попытка sql

---
+++ src/main/java/ru/mirea/webPetShop/myPets/Table.java
@@ -0,0 +1,11 @@
+package ru.mirea.webPetShop.myPets;
+
+public class Table {
+
+    public Table() {
+        // https://www.codejava.net/java-se/jdbc/connect-to-microsoft-sql-server-via-jdbc
+
+    }
+
+    public Line getEx
+}
--- src/main/java/ru/mirea/webPetShop/myPets/petService.java
+++ src/main/java/ru/mirea/webPetShop/myPets/petService.java
@@ -2,11 +2,17 @@ package ru.mirea.webPetShop.myPets;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class petService {
+
+    public petService() {
+
+    }
+
     List<Pet> pets() {
         List<Pet> pets = new LinkedList<Pet>();
         pets.add(new Pet(0, 5, "cat"));
